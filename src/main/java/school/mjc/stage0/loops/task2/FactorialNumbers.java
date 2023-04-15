b2fe20d635f0d1b755077de26559ca04daf3daa2
ElinaAndreeva <lolaclouds@yandex.ru>
2023-04-15 16:16:03 +0400

    Completed the loop tasks

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        System.out.println(0);
+        int fact = 1;
+        int multi = 1;
+        while (multi < printToInclusive) {
+            fact*=multi;
+            multi++;
+            System.out.println(fact);
+        }
     }
 }
